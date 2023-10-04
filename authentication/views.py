d004e562ada61e629e7432216d0fd734f1338b2a
Baklachok <korobko_01@inbox.ru>
2023-10-04 15:30:10 +0300

    made a logout working

--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.contrib.auth import logout
+from django.shortcuts import render, redirect
+
 
 def login_user(request):
     return render(request, 'auth/login.html')
 
 def logout_user(request):
-    pass
+    logout(request)
+    return redirect('index')
