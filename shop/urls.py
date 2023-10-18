7fb79fca3c02ec5a119061e24d4f4c797a9f9e2b
Baklachok <korobko_01@inbox.ru>
2023-10-18 13:24:00 +0300

    added cart

--- shop/urls.py
+++ shop/urls.py
@@ -6,5 +6,7 @@ from shop import views
 
 urlpatterns = [
     path('', views.ProductsListView.as_view(), name='shop'),
-    path('cart_view/', TemplateView.as_view(template_name='shop/cart.html'), name='cart-view'),
+    path('cart_view/', views.cart_view, name='cart-view'),
+    path('add-item-to-cart/<int:pk>', views.add_item_to_cart, name='add_item_to_cart'),
+    path('delete_item/<int:pk>', views.CartDeleteItem.as_view(), name='cart_delete_item')
 ]
