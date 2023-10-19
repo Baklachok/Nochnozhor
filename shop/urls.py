09a9d3e93c76882670a2bdd5d344954e85918c69
Baklachok <korobko_01@inbox.ru>
2023-10-19 18:23:05 +0300

    added make_order-view

--- shop/urls.py
+++ shop/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', views.ProductsListView.as_view(), name='shop'),
     path('cart_view/', views.cart_view, name='cart-view'),
     path('add-item-to-cart/<int:pk>', views.add_item_to_cart, name='add_item_to_cart'),
-    path('delete_item/<int:pk>', views.CartDeleteItem.as_view(), name='cart_delete_item')
+    path('delete_item/<int:pk>', views.CartDeleteItem.as_view(), name='cart_delete_item'),
+    path('make-order/', views.make_order, name='make_order'),
 ]
